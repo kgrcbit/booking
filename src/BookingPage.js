bba906373141b258dfaa82ec02284caa1eaa7624
Archithgandla <79374523+Archithgandla@users.noreply.github.com>
2023-11-14 00:43:07 +0530

    Update BookingPage.js
    Added scheduling logic

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -7,12 +7,31 @@ const BookingPage = () => {
   const [courtNumber, setCourtNumber] = useState('');
   const [timeSlot, setTimeSlot] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [status,setStatus] = useState([]);
+  //const [isAvailable,setIsAvailable] = useState(true);
+
+
+  //This variable represents the maximum number of courts
+  //(Here)The court number should be either 1 or 2 or 3
+  const numberOfCourts = 3;
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     if (!studentName || !courtNumber || !timeSlot ) {
       setErrorMessage('Please fill in all fields');
+      return;
+    }
+
+    if(courtNumber>numberOfCourts || courtNumber<=0){
+      setErrorMessage('please enter valid court number,courts 1,2 and 3 are available');
+      return;
+    }
+    
+    const slotCode = timeSlot+courtNumber;
+
+    if(status.includes(slotCode)){
+      setErrorMessage('Sorry the slot is not available, please choose another slot');;
       return;
     }
 
@@ -30,6 +49,7 @@ const BookingPage = () => {
         setCourtNumber('');
         setTimeSlot('');
         setErrorMessage('');
+        setStatus([...status,slotCode]);
       })
       .catch((error) => {
         console.log(error);
@@ -57,7 +77,9 @@ const BookingPage = () => {
             type="number"
             className="form-control"
             value={courtNumber}
-            onChange={(event) => setCourtNumber(event.target.value)}
+            onChange={(event) => {
+              setErrorMessage('');
+              setCourtNumber(event.target.value)}}
           />
         </div>
         <div className="mb-3">
@@ -65,15 +87,26 @@ const BookingPage = () => {
           <select
             className="form-select"
             value={timeSlot}
-            onChange={(event) => setTimeSlot(event.target.value)}
+            onChange={(event) => {
+              setErrorMessage('');  
+              setTimeSlot(event.target.value)}
+            }
           >
             <option value="">Select a time slot</option>
-            <option value="02:30-03:45">02:30-03:45</option>
-            <option value="03:45-04:30">03:45-04:30</option>
-            <option value="04:30-05:30">04:30-05:30</option>
+            <option value="a">02:30-03:45</option>
+            <option value="b">03:45-04:30</option>
+            <option value="c">04:30-05:30</option>
           </select>
         </div>
-          
+        
+        {/* {
+
+          !isAvailable && 
+          <div class="alert alert-warning" role="alert">
+            A simple warning alert—check it out!
+          </div>
+        } */}
+
         <button type="submit" className="btn btn-primary">Book Now</button>
       </form>
     </div>
